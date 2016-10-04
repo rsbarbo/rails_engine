3c42c8e3e5271ee88235ded0e20b191dae6ee8c2
Raphael Barbo <rsbarbo@gmail.com>
2016-10-04 15:16:59 -0600

    add foreign keys to invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004211220) do
+ActiveRecord::Schema.define(version: 20161004211611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20161004211220) do
     t.decimal  "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "item_id"
+    t.integer  "invoice_id"
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id", using: :btree
+    t.index ["item_id"], name: "index_invoice_items_on_item_id", using: :btree
   end
 
   create_table "invoices", force: :cascade do |t|
@@ -66,6 +70,8 @@ ActiveRecord::Schema.define(version: 20161004211220) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
