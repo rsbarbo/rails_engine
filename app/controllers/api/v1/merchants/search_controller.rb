984b7b58e996dbb460fb49eb8313b32fc0208b79
tgisg <soniagupta504@gmail.com>
2016-10-04 05:44:11 -0600

    add randomization and case insensitivity wip

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,22 +1,21 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    if params.keys.include?("name")
-      render json: Merchant.find_by("lower(#{checkparams}) = ?", params[checkparams].downcase)
-    else
+    if merchant_params.empty?
       render json:
       Merchant.limit(1).order("RANDOM()")
+    elsif merchant_params
+      render json: Merchant.find_by("lower(#{merchant_params.keys.first}) = ?", params[merchant_params.keys.first].downcase)
     end
   end
 
   def index
-    render json: Merchant.where("lower(#{checkparams}) = ?", params[checkparams].downcase)
+    render json: Merchant.where("lower(#{merchant_params.keys.first}) = ?", params[merchant_params.keys.first].downcase)
   end
 
   private
 
-  def checkparams
-    params.keys.first
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
-
 
 end
