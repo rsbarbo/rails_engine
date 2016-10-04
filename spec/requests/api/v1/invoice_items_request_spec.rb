56581730467739213b923e3aaae91bbfe7dfa34e
Raphael Barbo <rsbarbo@gmail.com>
2016-10-04 16:37:49 -0600

    update invoice item test and invoice items controller

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 describe "invoice_items actions" do
-  it "returns the item associated with an invoice_item" do
-    item = Fabricate(:item)
-    invoice_item = Fabricate(:invoice_item, item_id: item.id)
+  it "returns a list of all invoice items" do
+    3.times { Fabricate(:invoice_item) }
 
-    get "/api/v1/invoice_items/#{invoice_item.id}/item"
-    expect(response).to be_success
+    get "/api/v1/invoice_items.json"
 
-    invoice_item_parse = JSON.parse(response.body)
+    parse_invoice_items = JSON.parse(response.body)
 
-    expect(invoice_item_parse['name']).to eq(item.name)
-    expect(invoice_item_parse['id']).to eq(1)
+    expect(parse_invoice_items.count).to eq(3)
+
   end
 end
