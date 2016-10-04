e0c0567ebc7aba1d1549c16534017040ea0a47c2
Raphael Barbo <rsbarbo@gmail.com>
2016-10-03 23:39:06 -0600

    update item search controller with strong params and index, test pass

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
+    render json: Item.where(check_params)
   end
 
   def show
+  end
+
+  private
+  def check_params
+    params.permit(:id,
+                  :name,
+                  :description,
+                  :merchant_id,
+                  :created_at,
+                  :updated_at,
+                  :unit_price)
   end
 end
