a3898299a7ef55f2f3434ee02725fa1e1197afb2
Raphael Barbo <rsbarbo@gmail.com>
2016-10-04 17:02:56 -0600

    update unit price to integer rather than decimal

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,10 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  before_save :cents_to_dollar
+
+  def cents_to_dollar
+    byebug
+    # self.price_unit =
+  end
 end
