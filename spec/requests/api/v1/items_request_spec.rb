3f662347e282c1d3236e56f8f89aeaa58a994d6e
Raphael Barbo <rsbarbo@gmail.com>
2016-10-03 23:28:07 -0600

    add test and create item search controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,4 +18,31 @@ describe "items actions" do
     expect(response).to be_success
     expect(parse_item["name"]).to eq(item.name)
   end
+
+  it "finds a single item by single parameters" do
+    item = Fabricate(:item)
+
+    get "/api/v1/items/find?status=#{item.name}"
+
+    parse_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(parse_item["id"]).to eq(item.id)
+    expect(parse_item["customer_id"]).to eq(item.customer_id)
+    expect(parse_item["merchant_id"]).to eq(item.merchant_id)
+    expect(parse_item["status"]).to eq(item.status)
+  end
+
+  it "finds a multiple items by single parameters" do
+    item_1 = Fabricate(:item, status: "approved")
+    item_2 = Fabricate(:item, status: "approved")
+    item_3 = Fabricate(:item, status: "approved")
+
+    get "/api/v1/items/find_all?status=approved"
+
+    parse_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(parse_item.count).to eq(3)
+  end
 end
