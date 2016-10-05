449a449f8089ed670ce70225f79b3148b5697c0d
Raphael Barbo <rsbarbo@gmail.com>
2016-10-05 11:25:03 -0600

    update unit price back to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005164940) do
+ActiveRecord::Schema.define(version: 20161005171841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20161005164940) do
 
   create_table "invoice_items", force: :cascade do |t|
     t.integer  "quantity"
-    t.decimal  "unit_price", precision: 15, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.integer  "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "item_id"
     t.integer  "invoice_id"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id", using: :btree
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 20161005164940) do
   create_table "items", force: :cascade do |t|
     t.citext   "name"
     t.citext   "description"
-    t.decimal  "unit_price",  precision: 15, scale: 2
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.integer  "unit_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "merchant_id"
     t.index ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
   end
