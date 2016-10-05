9af28b2d0dfbf160d97dce50928372fe4721b931
tgisg <soniagupta504@gmail.com>
2016-10-05 11:05:48 -0600

    add transactions and invoice_items

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
         get "/find", to: "search#show"
         get "/find_all", to: "search#index"
         get "/random", to: "random#show"
+        get "/:id/transactions", to: "transactions#index"
+        get "/:id/invoice_items", to: "invoice_items#index"
       end
 
       namespace :items do
