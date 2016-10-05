b5e9aade9d2b1909b1c6b2daabf95682b7ff32d1
Raphael Barbo <rsbarbo@gmail.com>
2016-10-05 15:11:49 -0600

    update merchant relationshiop test"

--- spec/requests/api/v1/merchant_relationships_request_spec.rb
+++ spec/requests/api/v1/merchant_relationships_request_spec.rb
@@ -5,16 +5,13 @@ describe "merchant relationships" do
     merchant_1 = Fabricate(:merchant)
     merchant_2 = Fabricate(:merchant)
 
-    customer_pending_1 = load_data("/api/v1/merchants/#{merchant_1.id}/customers_with_pending_invoices")
-    customer_pending_2 = load_data("/api/v1/merchants/#{merchant_2.id}/customers_with_pending_invoices")
-
+    customer_pending_1 = get "/api/v1/merchants/#{merchant_1.id}/customers_with_pending_invoices"
+    customer_pending_2 = get "/api/v1/merchants/#{merchant_2.id}/customers_with_pending_invoices"
 
     expect(customer_pending_1.size).to eq(3)
     expect(customer_pending_2.size).to eq(5)
 
-    expect(customer_pending_1).to eq(197)
-    expect(customer_pending_2).to eq(28)
-    assert_response_has_attribute 197, pending_customer_one
-    assert_response_has_attribute 28,  pending_customer_two
+    expect(response).to have_attributes(197)
+    expect(response).to have_attributes(28)
   end
 end
