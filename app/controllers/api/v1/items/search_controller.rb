7d0715ea5eba47c92dad52cf05637cee56bbd475
Raphael Barbo <rsbarbo@gmail.com>
2016-10-05 11:58:22 -0600

    upate items search controller all spec harness passes now

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -9,12 +9,29 @@ class Api::V1::Items::SearchController < ApplicationController
 
   private
   def check_params
-    params.permit(:id,
-                  :name,
-                  :description,
-                  :merchant_id,
-                  :created_at,
-                  :updated_at,
-                  :unit_price)
+    clean_params = params.permit(:id,
+                                 :name,
+                                 :description,
+                                 :merchant_id,
+                                 :created_at,
+                                 :updated_at,
+                                 :unit_price)
+
+    clean_params_checker(clean_params)
+  end
+
+  def clean_params_checker(clean_params)
+    if clean_params[:unit_price]
+      return_formatted_unit_price(clean_params)
+    end
+    clean_params
+  end
+
+  def format_unit_price(unit_price)
+    unit_price.gsub('.', '')
+  end
+
+  def return_formatted_unit_price(clean_params)
+    clean_params[:unit_price] = format_unit_price(clean_params[:unit_price])
   end
 end
