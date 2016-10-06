7897dbb56ab960ee7c35abf7da0f7f0ed2c91347
tgisg <soniagupta504@gmail.com>
2016-10-06 15:11:08 -0600

    wip - business intelligence merchant revenue

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::Merchants::RevenuesController < ApplicationController
 
   def index
-    @merchant = Merchant.find(params[:id])
-    @revenue = {:revenue => revenue_across_all_transactions(@merchant).to_s}
-    render json: @revenue
+    merchant = Merchant.find(params[:id])
+    if params["date"]
+      revenue = {:revenue => merchant.revenue_by_date(params["date"]).to_s}
+    else
+      revenue = {:revenue => merchant.revenue_across_all_transactions.to_s}
+    end
+    render json: revenue
   end
 
-  private
 
-  def revenue_across_all_transactions(merchant)
-    merchant.invoice_items.pluck("quantity * unit_price").reduce(:+)
-  end
+
+
+
+
+
 
 end
--- spec/requests/api/v1/total_revenue_by_merchant_across_all_transactions_request_spec.rb
+++ spec/requests/api/v1/total_revenue_by_merchant_across_all_transactions_request_spec.rb
@@ -1,24 +1,45 @@
 require 'rails_helper'
 
 describe "total revenue by merchant across all transactions" do
-  it "returns total revenue by merchant across all transactions" do
+  # it "returns total revenue by merchant across all transactions" do
+  #   merchant = Fabricate(:merchant)
+  #   invoice1 = Fabricate(:invoice)
+  #   invoice2 = Fabricate(:invoice)
+  #   invoice1.update_attribute(:merchant_id, merchant.id)
+  #   invoice2.update_attribute(:merchant_id, merchant.id)
+  #   invoice_item1 = Fabricate(:invoice_item)
+  #   invoice_item2 = Fabricate(:invoice_item)
+  #   invoice_item1.update_attribute(:invoice_id, invoice1.id)
+  #   invoice_item2.update_attribute(:invoice_id, invoice2.id)
+  #
+  #   get "/api/v1/merchants/#{merchant.id}/revenue"
+  #
+  #   parsed_revenue = JSON.parse(response.body)
+  #
+  #   expect(response).to be_success
+  #   expect(parsed_revenue).to eq({"revenue" => ((invoice_item1.quantity * invoice_item1.unit_price) + (invoice_item2.quantity * invoice_item2.unit_price)).to_s})
+  # end
+
+  it "returns the total revenue for a single merchant for a specific invoice date" do
     merchant = Fabricate(:merchant)
     invoice1 = Fabricate(:invoice)
     invoice2 = Fabricate(:invoice)
     invoice1.update_attribute(:merchant_id, merchant.id)
-    invoice2.update_attribute(:merchant_id, merchant.id)
+    invoice2.update_attributes(:merchant_id => merchant.id, :created_at => invoice1.created_at)
     invoice_item1 = Fabricate(:invoice_item)
     invoice_item2 = Fabricate(:invoice_item)
     invoice_item1.update_attribute(:invoice_id, invoice1.id)
     invoice_item2.update_attribute(:invoice_id, invoice2.id)
+    date = invoice1.created_at
 
-    get "/api/v1/merchants/#{merchant.id}/revenue"
+    get "/api/v1/merchants/#{merchant.id}/revenue?date=#{date}"
 
     parsed_revenue = JSON.parse(response.body)
-
     expect(response).to be_success
-    expect(parsed_revenue).to eq((invoice_item1.quantity * invoice_item1.unit_price) + (invoice_item2.quantity * invoice_item2.unit_price))
+
   end
+
+
 
 
 end
