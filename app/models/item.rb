3f5ed44cced2f49bd8e61b16fda9af6e0ba1af6f
Raphael Barbo <rsbarbo@gmail.com>
2016-10-06 17:29:55 -0600

    update revenues controller and model item

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,9 @@ class Item < ApplicationRecord
     order("sum(invoice_items.quantity * invoice_items.unit_price) DESC").
     limit(quantity)
   end
+
+
+  def self.find_total_revenue_for_spec_date
+    byebug
+  end
 end
