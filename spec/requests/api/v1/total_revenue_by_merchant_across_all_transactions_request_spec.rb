13b28eca8f3e83cf0b91cac20c0081f0ab434e33
tgisg <soniagupta504@gmail.com>
2016-10-06 16:33:52 -0600

    fix total revenue by merchant test - wip

--- spec/requests/api/v1/total_revenue_by_merchant_across_all_transactions_request_spec.rb
+++ spec/requests/api/v1/total_revenue_by_merchant_across_all_transactions_request_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 describe "total revenue by merchant across all transactions" do
   it "returns total revenue by merchant across all transactions" do
-    merchant = Fabricate(:merchant, name: "Amazon", id: 4)
-    invoice1 = Fabricate(:invoice, id: 1, merchant_id: merchant.id)
-    invoice2 = Fabricate(:invoice, id: 2, merchant_id: merchant.id)
+    merchant = Fabricate(:merchant)
+    invoice1 = Fabricate(:invoice)
+    invoice2 = Fabricate(:invoice)
+    invoice1.update_attribute(:merchant_id, merchant.id)
+    invoice2.update_attribute(:merchant_id, merchant.id)
     transaction1 = Fabricate(:transaction, result: "success")
     transaction2 = Fabricate(:transaction, result: "success")
     transaction1.update_attribute(:invoice_id, invoice1.id)
     transaction2.update_attribute(:invoice_id, invoice2.id)
-    invoice_item1 = Fabricate(:invoice_item, quantity: 2, unit_price: 10055, invoice_id: invoice1.id)
-    invoice_item2 = Fabricate(:invoice_item, quantity: 2, unit_price: 20055, invoice_id: invoice2.id)
-    # invoice_item1.update_attribute(:invoice_id, invoice1.id)
-    # invoice_item2.update_attribute(:invoice_id, invoice2.id)
+    invoice_item1 = Fabricate(:invoice_item, quantity: 2, unit_price: 10055)
+    invoice_item2 = Fabricate(:invoice_item, quantity: 2, unit_price: 20055)
+    invoice_item1.update_attribute(:invoice_id, invoice1.id)
+    invoice_item2.update_attribute(:invoice_id, invoice2.id)
     get "/api/v1/merchants/#{merchant.id}/revenue"
 
     parsed_revenue = JSON.parse(response.body)
