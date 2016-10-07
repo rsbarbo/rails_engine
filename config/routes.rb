1713a8de41032c4903d889744021d6318cb4bdcc
Raphael Barbo <rsbarbo@gmail.com>
2016-10-06 21:32:33 -0600

    update routes and create merchants items controller

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
         get "/:id/favorite_customer", to: "favorite_customer#show"
         get "/:id/customers_with_pending_invoices", to: "customers_with_pending_invoices#index"
         get '/:id/invoices', to: 'invoices#index'
+        get '/:id/items', to: 'items#index'
       end
 
       namespace :transactions do
