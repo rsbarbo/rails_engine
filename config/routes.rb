b629394c643fd69a72d7a2a95c77927e3d1a9cb4
Raphael Barbo <rsbarbo@gmail.com>
2016-10-06 20:49:45 -0600

    update routes and create transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
         get "/random", to: "random#show"
         get "/find_all", to: "search#index"
         get '/:id/invoices', to: 'invoices#index'
+        get '/:id/transactions', to: 'transactions#index'
       end
 
       namespace :invoice_items do
