61824b3549361955bf3ea7d2a740744bd1985ad5
Raphael Barbo <rsbarbo@gmail.com>
2016-10-06 19:34:20 -0600

    update test, now rspec passes 100 %

--- spec/requests/api/v1/business_intel_items_most_revenue_by_qty_request_spec.rb
+++ spec/requests/api/v1/business_intel_items_most_revenue_by_qty_request_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe "The most revenue for items" do
   it "give back the top number of merchants organized by revenue" do
-    merchant_1 = Fabricate(:merchant, id: 1)
-    merchant_2 = Fabricate(:merchant, id: 2)
-    merchant_3 = Fabricate(:merchant, id: 3)
+    merchant_1 = Fabricate(:merchant)
+    merchant_2 = Fabricate(:merchant)
+    merchant_3 = Fabricate(:merchant)
 
-    invoice_1  = Fabricate(:invoice, id: 1, merchant_id: merchant_1.id)
-    invoice_2 = Fabricate(:invoice, id: 2, merchant_id: merchant_2.id)
-    invoice_3 = Fabricate(:invoice, id: 3, merchant_id: merchant_3.id)
+    invoice_1  = Fabricate(:invoice, merchant_id: merchant_1.id)
+    invoice_2 = Fabricate(:invoice, merchant_id: merchant_2.id)
+    invoice_3 = Fabricate(:invoice, merchant_id: merchant_3.id)
 
     Fabricate(:invoice_item, invoice_id: invoice_1.id, unit_price: 9000, quantity: 1)
     Fabricate(:transaction, invoice_id: invoice_1.id, result: "success")
