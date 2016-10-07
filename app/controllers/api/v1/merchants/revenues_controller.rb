1eb337ae2dc5d294d52507ba8e030ea654b50b8f
Raphael Barbo <rsbarbo@gmail.com>
2016-10-06 18:04:58 -0600

    update revenues controller

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::RevenuesController < ApplicationController
   def index
     @total_revenue_for_spec_date = Merchant.find_total_revenue_for_spec_date(params[:date])
-    render json: @total_revenue_for_spec_date
+    render json: @total_revenue_for_spec_date, serializer: MerchantRevenuesSerializer
   end
 end
